4c7acfb0e7926c9710a7342b6660988e0888765d
Greg Rodriguez <greg_orio15323@yahoo.com>
2021-10-12 12:34:04 -0600

    contact title

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-
+import { Section, SectionDivider, SectionTitle, SectionText } from '../../styles/GlobalComponents';
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
 const Footer = () => {
   return (
     <FooterWrapper nopadding id="footer">
-      <LinkList>
-       <LinkColumn>
+      <SectionTitle main>Contact</SectionTitle>
+      <SectionText>
+        Lets work together  
+      </SectionText>
+      <LinkColumn>
         <LinkTitle>Phone</LinkTitle>
         <LinkItem href="tel:435-225-6099">435-225-6099</LinkItem>
        </LinkColumn>
@@ -16,6 +19,7 @@ const Footer = () => {
         <LinkTitle>Email</LinkTitle>
         <LinkItem href="mailto:gsrconstructionut@gmail.com">GSRCONSTRUCTIONUT@GMAIL.COM</LinkItem>
        </LinkColumn>
+      <LinkList>
       </LinkList>
       <CompanyContainer>
         <Slogan>GSR CONSTRUCTION © 2021 | <LinkItem href="https://www.linkedin.com/in/greg-rodriguez-5936331b5/">Designed by Greg Rodriguez</LinkItem>
