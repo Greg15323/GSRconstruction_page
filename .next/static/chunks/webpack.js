6d4e0b141e1970b1abd6c454abdacb6acba17e71
Greg Rodriguez <greg_orio15323@yahoo.com>
2021-10-19 13:51:27 -0600

    added description to projects card

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0b37fb39fff434bc9c56"; }
+/******/ 		__webpack_require__.h = function() { return "5dfadc8049d8f566a13e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 export const projects = [
   {
     title: 'Crispy Cones',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+    description: "Metal framing was used to build the inside of Crispy Cones located in Logan, UT.",
       image: '/images/crispycones.jpeg',
       tags: ['Metal Framing'],
     source: 'https://google.com',
